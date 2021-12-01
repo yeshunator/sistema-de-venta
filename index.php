6774ba9d2385afedad173bdbed2863c7ca4893fb
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2021-12-01 18:07:01 -0500

    v.0.0

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
     $ruta = !empty($_GET['url']) ? $_GET['url'] : "Home/index";
+    $array = explode("/", $ruta);
+    $controller = $array[0];
+    $metodo = "index";
+    $parametro = "";
+    if (!empty($array[1])) {
+        if (!empty($array[1] != "")) {
+            $metodo = $array[1];
+        }
+    }
+    if (!empty($array[2])) {
+        if (!empty($array[2] != "")) {
+            for ($i=2; $i < count($array); $i++) { 
+                $parametro .= $array[$i]. ",";
+            }
+        }
+    }
+    echo $controller;
+    echo $metodo;
+    echo $parametro;
 
-echo $ruta;
+
 
 ?>
