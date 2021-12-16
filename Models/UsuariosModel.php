ef263b3c8aa9e3d5811bdafa1f17121ea81c083e
yeshuaaguilar <76400533@certus.edu.pe>
2021-12-16 18:23:31 -0500

    v.0.3

---
+++ Models/UsuariosModel.php
@@ -0,0 +1,15 @@
+<?php
+class UsuariosModel extends Query{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function getUsuario()
+    {
+        $sql = "SELECT * FROM usuarios";
+        $data = $this->select($sql);
+        return $data;
+    }
+}
+
+?>
